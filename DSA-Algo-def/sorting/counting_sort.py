589d1d3d7032a9c0ba9cbba9acfe0e627f66e29e
MoeMoeee <dangkhoa.140502@gmail.com>
2023-03-14 23:05:17 +1100

    Add implementation for counting sort algorithm and test case

--- DSA-Algo-def/sorting/counting_sort.py
+++ DSA-Algo-def/sorting/counting_sort.py
@@ -7,10 +7,34 @@
 # Average	        O(n+u)
 # Space Complexity	O(n+u) for best, worst, average
 # Stability	        Yes
-def counting_sort(arr, size):
 
-    return
+def counting_sort(arr, size):
+    # first create the result list to hold using the given size
+    result = [0]*len(arr)
+    counter_arr = [0]*(size+1)  # temp to count the occurrences
+
+    # find the occurrences of each element
+    for i in range(0, len(arr)):
+        ele = arr[i]
+        counter_arr[ele] += 1
+        # counter_arr = [0, 1, 2, 1, 0, 1, 0, 1, 0, 1]
+
+    # increment the occurrences
+    for i in range(1, len(counter_arr)):
+        counter_arr[i] = counter_arr[i-1] + counter_arr[i]
+        # [0, 1, 3, 4, 4, 5, 5, 6, 6, 7]
+
+    for i in range(len(arr)):
+        element = arr[i]
+        temp_index = counter_arr[element]
+        # temp_index - 1 because array index starts from 0
+        result[temp_index-1] = element
+        counter_arr[element] -= 1
+
+    return result
 
 
 if __name__ == "__main__":
-    print(counting_sort([3, 2, 5, 1, 7, 9], 6))
+    arr = [0, 3, 2, 5, 1, 2, 7, 9]
+    max_val = max(arr)
+    print(counting_sort(arr, max_val))
