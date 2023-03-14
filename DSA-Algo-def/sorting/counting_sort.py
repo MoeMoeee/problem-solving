77bb74a96cf2a52e907d81faf23e3c0a9fd40378
MoeMoeee <dangkhoa.140502@gmail.com>
2023-03-14 21:24:40 +1100

    Add file counting_sort.py and update file quicksort.txt

---
+++ DSA-Algo-def/sorting/counting_sort.py
@@ -0,0 +1,16 @@
+# Counting sort is a sorting algorithm that sorts the elements of an array by counting the
+# number of occurrences of each unique element in the array.
+
+# Time Complexity
+# Best	            O(n+u)  (u is size)
+# Worst	            O(n+u)
+# Average	        O(n+u)
+# Space Complexity	O(n+u) for best, worst, average
+# Stability	        Yes
+def counting_sort(arr, size):
+
+    return
+
+
+if __name__ == "__main__":
+    print(counting_sort([3, 2, 5, 1, 7, 9], 6))
