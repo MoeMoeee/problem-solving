3fc0e4c3c6bb60178a489ece646cd68769a2043c
MoeMoeee <dangkhoa.140502@gmail.com>
2023-03-21 13:54:29 +1100

    Add delete_nth function and test cases

---
+++ codewars/delete_nth.py
@@ -0,0 +1,11 @@
+def delete_nth(lst, nth):
+    result = []
+    # code here
+    return result
+
+
+if __name__ == '__main__':
+    print(delete_nth([1, 1, 3, 3, 7, 2, 2, 2, 2], 3))
+    # -> [1, 1, 3, 3, 7, 2, 2, 2]
+    print(delete_nth([20, 37, 20, 21], 1))
+    # -> [20,37,21]
