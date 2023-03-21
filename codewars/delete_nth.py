7c2ad7e974c6d765e85ab43642becd5f4c66ab4b
MoeMoeee <dangkhoa.140502@gmail.com>
2023-03-21 17:01:35 +1100

    finished delete_nth function

--- codewars/delete_nth.py
+++ codewars/delete_nth.py
@@ -1,11 +1,17 @@
-def delete_nth(lst, nth):
+
+def delete_nth(lst, max):
     result = []
-    # code here
+    count = {}
+
+    for item in lst:
+        if item not in count:
+            count[item] = 0
+        if count[item] < max:
+            result.append(item)
+            count[item] += 1
+
     return result
 
 
 if __name__ == '__main__':
     print(delete_nth([1, 1, 3, 3, 7, 2, 2, 2, 2], 3))
-    # -> [1, 1, 3, 3, 7, 2, 2, 2]
-    print(delete_nth([20, 37, 20, 21], 1))
-    # -> [20,37,21]
