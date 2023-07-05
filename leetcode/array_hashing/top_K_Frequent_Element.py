e029cafbda14aa95acaac7e7f410069d9b4068d5
Ba Dang Khoa Pham <dangkhoa.140502@gmail.com>
2023-07-05 12:58:01 +0700

    added document for top K element

--- leetcode/array_hashing/top_K_Frequent_Element.py
+++ leetcode/array_hashing/top_K_Frequent_Element.py
@@ -8,17 +8,19 @@ class Solution(object):
         :rtype: List[int]
         """
         result = []
-        group = defaultdict(int)
+        group = defaultdict(int) #key: num || value: occurence
         
-        
+        # to count the occurence 
         for num in nums:
             group[num] += 1
-            
+        
+        # sort the values acscending
         sorted_values = sorted(group.values(), reverse = True)
         
+        # only get the k_th element 
         k_element = sorted_values[:k]
         
-        
+        # get the keys(num) that match the occurence
         for key in group.keys():
             if group[key] in k_element:
                 result.append(key)
