ee4356a39aec8e06d83318bd53055a5352ab05ed
Ba Dang Khoa Pham <dangkhoa.140502@gmail.com>
2024-02-03 23:46:48 +0700

    added solution for sell stock using sliding window

---
+++ leetcode/3. sliding_window/longest_substring_without_repeat.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        #TODO
+
+
+        
+        
+
+
+
+
+if __name__ == '__main__':
+    a = Solution()
+    s = [7,1,5,3,6,4]
+    print(a.lengthOfLongestSubstring(s))
---
+++ leetcode/3. sliding_window/sell_stock.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if not prices:
+            return 0
+        
+        max_profit = 0
+        window_min = prices[0]
+        
+        for price in prices:
+            window_min = min(window_min, price)
+            max_profit = max(max_profit, price - window_min)
+
+        return max_profit
+
+
+        
+        
+
+
+
+
+if __name__ == '__main__':
+    a = Solution()
+    prices = [7,1,5,3,6,4]
+    print(a.maxProfit(prices))
+    
+    
